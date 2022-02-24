6c39e14880b69b300327d3d4d2de9d9057850af0
Joana Moufat <joana.moufat@gmail.com>
2022-02-24 16:12:08 +0800

    structure done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+
+user_serialized = URI.open(url).read
+movies = JSON.parse(user_serialized)
+
+movies["results"].each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}")
+end
