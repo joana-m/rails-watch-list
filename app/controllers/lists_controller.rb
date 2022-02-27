f53b3b59b168bad15b3fa2e36548a07a2665fada
Joana Moufat <joana.moufat@gmail.com>
2022-02-27 12:22:05 +0800

    design watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @bookmarks = Bookmark.where(params[:list_id])
+    @bookmarks = @list.bookmarks
   end
 
   def new
@@ -24,6 +24,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
